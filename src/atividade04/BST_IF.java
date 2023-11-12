a2294b8ff1236c1b1aab21ac76efaac819de2f01
Everaldo-Martins <everaldoinfortecnico@gmail.com>
2023-11-12 17:14:09 -0300

    Update

--- src/atividade04/BST_IF.java
+++ src/atividade04/BST_IF.java
@@ -1,19 +1,11 @@
 package atividade04;
 
 /*
-<<<<<<< HEAD
  * Interface para implementação da classe "BST".
  */
 public interface BST_IF {
 	public void insert(Integer element);
 	public Integer search(Integer element) throws Exception; //lançar exceção caso não encontre
-=======
- * Interface para implementa��o da classe "BST".
- */
-public interface BST_IF {
-	public void insert(Integer element);
-	public int search(Integer element) throws Exception; //lan�ar exce��o caso n�o encontre
->>>>>>> 7a26361589734c9be39d51faef62b957f1506403
 	public int[] preOrder();
 	public int[] order();
 	public int[] postOrder();
