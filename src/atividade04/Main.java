e7fc5eeb9582dbfff52db6b553523f608478eba2
Everaldo-Martins <everaldoinfortecnico@gmail.com>
2023-11-15 20:49:59 -0300

    Update Main

--- src/atividade04/Main.java
+++ src/atividade04/Main.java
@@ -62,8 +62,7 @@ public class Main {
         // Busca por valores
         System.out.println("Valor para chave 10: " + arvorePV.get(5));
         System.out.println("Valor para chave 5: " + arvorePV.get(10));
-        System.out.println("Valor para chave: " +arvorePV.get(30));
-        System.out.println("Valor para chave: " +arvorePV.get(7));
-
+        System.out.println("Valor para chave: " + arvorePV.get(15));
+        System.out.println("Valor para chave: " + arvorePV.get(7));
     }
 }
